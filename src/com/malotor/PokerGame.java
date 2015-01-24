3c44b2c18842467e6e0ce3ae77d2abdaf6923984
Manel <malotor@gmail.com>
2015-01-24 11:59:16 +0000

    Test only 1 card GREEN

--- src/com/malotor/PokerGame.java
+++ src/com/malotor/PokerGame.java
@@ -3,8 +3,22 @@ package com.malotor;
 
 public class PokerGame {
 
-
     public int resolve(String[] hands) {
-        return 2;
+
+        int highNumber = 0;
+        int winner = 0;
+        int currentPlayer = 1;
+
+        for (String card : hands) {
+            char number = card.charAt(0);
+            //char number = card.charAt(0);
+            if (number > highNumber ) {
+                highNumber = number;
+                winner = currentPlayer;
+            }
+            currentPlayer++;
+        }
+
+        return winner;
     }
 }
